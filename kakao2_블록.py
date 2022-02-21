67ab8b219e7600540137cb50c9ad6173428cc0e7
KIMTAEKWAN <twan4220@gmail.com>
2022-02-22 02:54:01 +0900

    Update kakao2_블록.py

--- kakao2_블록.py
+++ kakao2_블록.py
@@ -1,6 +1,16 @@
-def solution(m, n, board):
-    answer = 0
-    return answer
+N = int(input())
+field = [list(map(int, input().split())) for _ in range(N)]
 
+df = [[0] * N for _ in range(N)] 
+df[0][0] = 1
 
-print('dfdf')
+for i in range(N):
+    for j in range(N):
+        if (i is N-1) & (j is N-1): 
+            print(df[i][j])
+            break
+        tmp = field[i][j]
+        if j + tmp < N:
+            df[i][j + tmp] += df[i][j]
+        if i + tmp < N:
+            df[i + tmp][j] += df[i][j]
