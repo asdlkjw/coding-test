b37f574097d9bebd84d5a9c1004b545eec6b2ed9
KIMTAEKWAN <twan4220@gmail.com>
2022-02-24 05:37:25 +0900

    Update kaka2_문자정렬.py

--- kaka2_문자정렬.py
+++ kaka2_문자정렬.py
@@ -1,22 +1,52 @@
-
-
+import re
 def quick_sort(array):
 
     if len(array) <= 1:
         return array
-
+    
     pivot = array[0]
     tail = array[1:]
-
-    left_side = [x for x in tail if x <= pivot]
-    right_side = [x for x in tail if x > pivot]
-
-    # left_side = [array2[tail.index(x)] for x in tail if x <= pivot]
-    # right_side = [array2[tail.index(x)] for x in tail if x > pivot]
+    
+    left_side = [x for x in tail if int(re.sub(r'[^0-9]', '', x)) < int(re.sub(r'[^0-9]', '', pivot))]
+    right_side = [x for x in tail if int(re.sub(r'[^0-9]', '', x)) >= int(re.sub(r'[^0-9]', '', pivot))]
 
     return quick_sort(left_side) + [pivot] + quick_sort(right_side)
 
-array= [9,3,345,53]
-array2 = ['b','a','d2ec','c']
-print(quick_sort(array2))
+def solution(files):
+    
+    files3 = [x.split('.')[0] for x in files]
+    top = sorted(list(set([ x[:1].lower() for x in files ])))
+    
+    answer = []
+    if len(top) > 1:
+        files2 = sorted(word.lower() for word in files3)
+        top = sorted(list(set([x[:1] for x in files2])))
+    else:
+        files2 = [word.lower() for word in files3]
+        
+    
+    files2 += '_'
+    best = []
+    for i in top:
+        tmp = 0
+        check = False
+        for j,k in enumerate(files2):
+            if k[0] == i:
+                if check != True:
+                    tmp = j
+                check = True
+                pass
+            elif check != False:
+                best.append(files2[tmp:j])
+                break
+    
+    for i in range(len(best)):
+        
+        answer += quick_sort(best[i])
+    
+    for i in files:
+        j = i.split('.')[0]
+        answer[answer.index(j.lower())] = i
 
+    print(answer)
+    return answer
