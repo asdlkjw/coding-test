767aae17d9d7f5d749249ad9b04f61cdb1c620d7
KIMTAEKWAN <twan4220@gmail.com>
2022-03-02 22:01:57 +0900

    Update kakao2_주차요금.py

---
+++ kakao2_주차요금.py
@@ -0,0 +1,17 @@
+def solution(fees, records):
+    answer = []
+    record = []
+    for i in records:
+        record.append(i.split())
+    for i in range(len(record)):
+        h = int(record[i][0].split(":")[0])*60
+        m = int(record[i][0].split(":")[1])
+        record[i].append(h+m)
+        
+    for i in record:
+        for j in record:
+            if i[1] == j[1]:
+                fee = round(fees[0]-abs(i[3]-j[3]), 0)
+                print(fee)
+    print(fees, record)
+    return answer
