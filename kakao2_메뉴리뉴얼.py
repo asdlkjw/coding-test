20b4b7e2bc7631aa62f67d805e22615f2cd3bbdd
KIMTAEKWAN <twan4220@gmail.com>
2022-03-13 23:50:00 +0900

    Update kakao2_메뉴리뉴얼.py

--- kakao2_메뉴리뉴얼.py
+++ kakao2_메뉴리뉴얼.py
@@ -1,4 +1,33 @@
 def solution(orders, course):
+    from itertools import combinations
     answer = []
-    print(orders, course)
-    return answer
+    ap = []
+    for i in orders:
+        for j in i:
+            ap.append(j)
+    ap = sorted(list(set(ap)))
+    
+    for cour in course:
+        comb = list(combinations(ap, cour))
+    
+        t1 = [0]*len(comb)
+
+        for order in orders:
+            for co,com in enumerate(comb):
+                tmp = 0
+                for i in com:
+                    if i in order:
+                        tmp +=1
+                if tmp == cour:
+                    t1[co] += 1
+
+        t_max = max(t1)
+        if t_max >= 2:
+            t_mlist = [i for i,j in enumerate(t1) if j == t_max]
+
+            for t_m in t_mlist:
+                result = comb[t_m]
+                result = ''.join(result)
+                answer.append(result)
+
+    return sorted(answer)
