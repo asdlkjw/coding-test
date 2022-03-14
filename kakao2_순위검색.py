2423cb9aaf15059126d832124ae6231c47e59149
KIMTAEKWAN <twan4220@gmail.com>
2022-03-14 21:33:18 +0900

    Create kakao2_순위검색.py

---
+++ kakao2_순위검색.py
@@ -0,0 +1,11 @@
+def solution(info, query):
+    answer = []
+    
+    info = [inf.split() for inf in info]
+    print(info)
+    print()
+    query = [quer.split(' and ') for quer in query if ]
+    
+    print(query)
+    
+    return answer
