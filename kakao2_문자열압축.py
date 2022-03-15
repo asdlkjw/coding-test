87abc4fa87fbcb9044ceca8697553158b305dd79
KIMTAEKWAN <twan4220@gmail.com>
2022-03-15 22:30:58 +0900

    Update kakao2_문자열압축.py

--- kakao2_문자열압축.py
+++ kakao2_문자열압축.py
@@ -1,12 +1,10 @@
 def solution(s):
-    # from collections import Counter
-    # print(Counter(s))
     answer = 0
     import re
-    for i in range(1,len(s)+1):
-        a = f'{s[:i]}'
-        print(re.split(rf'{(a)}+', s))
-        print(a)
+    from collections import Counter
+    a = {4}
+    b = (re.findall(fr'[a-z]{a}',s))  
+    print(b)
+    print(Counter(b))
     
-    s[0][:2]    
     return answer
