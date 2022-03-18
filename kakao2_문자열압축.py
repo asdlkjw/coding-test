167925d331e372606d97aebede0c9b559c3ef426
KIMTAEKWAN <twan4220@gmail.com>
2022-03-18 23:49:17 +0900

    Update kakao2_문자열압축.py

--- kakao2_문자열압축.py
+++ kakao2_문자열압축.py
@@ -76,3 +76,23 @@ def solution(s):
                     else:
                         b = b + tmp result.append(len(b))
                         return min(result)
+def solution(s):
+    answer=10000
+    for n in range(1,len(s)//2+1):
+        res=''
+        cnt=1
+        tmp=s[:n] #단위문자열 초기화
+
+        for i in range(n,len(s)+n,n):
+            if tmp == s[i:i+n]:
+                cnt+=1
+            else:
+                if cnt==1:
+                    res+=tmp
+                else:
+                    res+=str(cnt)+tmp
+                tmp=s[i:i+n]
+                cnt=1
+        #print(res)
+        answer=min(answer,len(res))
+    return answer
