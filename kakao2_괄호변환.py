69fa5159202a8f0cc901f8270f56e255a6c956a7
KIMTAEKWAN <twan4220@gmail.com>
2022-03-23 00:37:04 +0900

    Update kakao2_괄호변환.py
    best 풀이 옳은 괄호는 파이썬에서 True로 반환 한다

--- kakao2_괄호변환.py
+++ kakao2_괄호변환.py
@@ -46,3 +46,19 @@ def solution(p):
                 
         
     return answer
+
+
+##########best 풀이#################### 
+
+def solution(p):
+    if p=='': return p
+    r=True; c=0
+    for i in range(len(p)):
+        if p[i]=='(': c-=1
+        else: c+=1
+        if c>0: r=False
+        if c==0:
+            if r:
+                return p[:i+1]+solution(p[i+1:])
+            else:
+                return '('+solution(p[i+1:])+')'+''.join(list(map(lambda x:'(' if x==')' else ')',p[1:i]) ))
