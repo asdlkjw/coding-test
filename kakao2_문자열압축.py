e5e48806d0f2e9d0a30fc046a5f2e3ef644bce90
KIMTAEKWAN <twan4220@gmail.com>
2022-03-23 00:58:29 +0900

    Update kakao2_문자열압축.py

--- kakao2_문자열압축.py
+++ kakao2_문자열압축.py
@@ -11,7 +11,7 @@ def solution(s):
 
 def solution(s):
     answer=10000
-    for n in range(1,len(s)//2+1):
+    for n in range(1,len(s)//2+2):
         res=''
         cnt=1
         tmp=s[:n] #단위문자열 초기화
@@ -29,3 +29,37 @@ def solution(s):
         #print(res)
         answer=min(answer,len(res))
     return answer
+
+
+
+###########베스트 풀이 ################ 별로임 코드 걍 복잡하기만 함 그냥 겉멋만 듬
+
+def compress(text, tok_len):
+    words = [text[i:i+tok_len] for i in range(0, len(text), tok_len)]
+    res = []
+    cur_word = words[0]
+    cur_cnt = 1
+    for a, b in zip(words, words[1:] + ['']):
+        if a == b:
+            cur_cnt += 1
+        else:
+            res.append([cur_word, cur_cnt])
+            cur_word = b
+            cur_cnt = 1
+    return sum(len(word) + (len(str(cnt)) if cnt > 1 else 0) for word, cnt in res)
+
+def solution(text):
+    return min(compress(text, tok_len) for tok_len in list(range(1, int(len(text)/2) + 1)) + [len(text)])
+
+a = [
+    "aabbaccc",
+    "ababcdcdababcdcd",
+    "abcabcdede",
+    "abcabcabcabcdededededede",
+    "xababcdcdababcdcd",
+
+    'aaaaaa',
+]
+
+for x in a:
+    print(solution(x))
