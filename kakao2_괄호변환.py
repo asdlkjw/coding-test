1a3ecc230f89be54ccd60c9e57236c2622c4651f
KIMTAEKWAN <twan4220@gmail.com>
2022-03-23 00:03:44 +0900

    Update kakao2_괄호변환.py

--- kakao2_괄호변환.py
+++ kakao2_괄호변환.py
@@ -3,8 +3,32 @@ def solution(p):
     if p == answer:
         return answer
     
-    for i in range(2,len(p), 2):
-        print(p[:i])
-    # print(p)
+    def dive(v):
+        if v == '':
+            return ''
+        for i in range(2, len(v)+2, 2):
+            if (v[:i]).count('(') == v[:i].count(')'):
+                u, x = v[:i], v[i:]   
+                if x == '':
+                    return u
+                
+                if u == True:
+                      x = dive(x)
+                    w = u + x   
+                    return w
+            
+    
+    for i in range(2,len(p)+2, 2):
+        if (p[:i].count('(') == p[:i].count(')')):
+            u ,v = p[:i], p[i:]
+            
+            if u == True:
+                u, v = dive(v)
+                
+            else:
+                
+                '(' + v ')' + reverse u[1:-2]
+                
+                
+        
     return answer
-
