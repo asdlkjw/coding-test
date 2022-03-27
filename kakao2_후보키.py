854b00babf79795c7a0d868e359db257a18d91b1
KIMTAEKWAN <twan4220@gmail.com>
2022-03-27 15:13:49 +0900

    Update kakao2_후보키.py

--- kakao2_후보키.py
+++ kakao2_후보키.py
@@ -78,3 +78,31 @@ def solution(relation):
             if not_duplicate:
                 answer_list.append(i)
     return len(answer_list)
+
+
+        # if len(final[i]) == len(set(final[i]).intersection(set(final[j]))): # key 중에 겹치는 부분이 있는 것을 삭제
+        #         answer.discard(final[j])''
+        
+from collections import deque
+from itertools import combinations
+def solution(relation):
+    n_row=len(relation)
+    n_col=len(relation[0])  #->runtime error 우려되는 부분
+
+    candidates=[]
+    for i in range(1,n_col+1):
+        candidates.extend(combinations(range(n_col),i))
+
+    final=[]
+    for keys in candidates:
+        tmp=[tuple([item[key] for key in keys]) for item in relation]
+        if len(set(tmp))==n_row:
+            final.append(keys)
+
+    answer=set(final[:])
+    for i in range(len(final)):
+        for j in range(i+1,len(final)):
+            if len(final[i])==len(set(final[i]).intersection(set(final[j]))):   # intersection,  discard 함수를 찾아보기
+                answer.discard(final[j])
+
+    return len(answer)
