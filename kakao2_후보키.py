7a899224ce9b241bc019a1404a8121843dd5e99e
KIMTAEKWAN <twan4220@gmail.com>
2022-03-27 14:17:56 +0900

    Update kakao2_후보키.py

--- kakao2_후보키.py
+++ kakao2_후보키.py
@@ -2,11 +2,12 @@ def solution(relation):
     from itertools import combinations
 
     answer = []
+    answer2= []
     rel = len(relation)
     att = len(relation[0])
     
     tmp = [[] for _ in range(att)]
-    
+    test = []
     for i in range(att):
         for j in relation:
             tmp[i].append(j[i])
@@ -14,7 +15,18 @@ def solution(relation):
         if len(list(set(tmp[i]))) == rel:
             answer.append(i)
         else:
-            print(i)
+            test.append(i)
+        
+    for i in range(2, len(test)+1):
+        for j in (list(combinations(test, i))):
+            temp = [[] for _ in range(rel)]
+            for k in j:
+                for z,p in enumerate(tmp[k]):
+                    temp[z].append(p)
+            if len(list(set([''.join(temp[q]) for q in range(len(temp))]))) == rel:
+                answer2.append(j)
     
-    print(answer)
-    return 2
+    for i in answer2:
+        if len(i) == min([len(i) for i in answer2]):
+            answer.append(i)
+    return len(answer)
