7fbd0ddad3b60dd2dad05f9ca2440901ca063908
KIMTAEKWAN <twan4220@gmail.com>
2022-03-28 22:46:20 +0900

    고득점 kit

---
+++ 고득점kit/해쉬/완주하지못한선수.py
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#정석 풀이
+
+def solution(participant, completion):
+    answer = ''
+    temp = 0
+    dic = {}
+    for part in participant:
+        dic[hash(part)] = part
+        temp += int(hash(part))
+    for com in completion:
+        temp -= hash(com)
+    answer = dic[temp]
+
+    return answer
+
