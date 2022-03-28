3410fd9475f5878b85ca2bc775d855bd4ed88a4c
KIMTAEKWAN <twan4220@gmail.com>
2022-03-28 22:49:34 +0900

    Update 완주하지못한선수.py

--- 고득점kit/해쉬/완주하지못한선수.py
+++ 고득점kit/해쉬/완주하지못한선수.py
@@ -1,20 +1,16 @@
+# 내 풀이 10분컷
+def solution(participant, completion):
+    from collections import Counter
+    for i,j in dict(Counter(participant + completion)).items():
+        if j%2 : answer = i
+    return answer
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# 베스트 풀이   dict 은 사칙연산이 안되지만 Counter 객체는 뺄셈이 가능하다
+import collections
+def solution(participant, completion):
+    answer = collections.Counter(participant) - collections.Counter(completion)
+    return list(answer.keys())[0]
 
 
 
