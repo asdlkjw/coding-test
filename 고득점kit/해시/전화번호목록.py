b6ebf7b5e33e4d3f1b5b5ec28bcb1e8a881655f8
KIMTAEKWAN <twan4220@gmail.com>
2022-03-29 23:35:13 +0900

    Update 전화번호목록.py

--- 고득점kit/해시/전화번호목록.py
+++ 고득점kit/해시/전화번호목록.py
@@ -15,3 +15,14 @@ def solution(phone_book):
                 answer = False 
                 break
     return answer
+
+
+def solution(phoneBook):
+    phoneBook = sorted(phoneBook)
+
+    for p1, p2 in zip(phoneBook, phoneBook[1:]):
+        if p2.startswith(p1):
+            return False
+    return True
+
+# 깔끔한 풀이  startswith 라는 함수를 활용  zip으로  한칸 밀어서 합쳐서 사용함
