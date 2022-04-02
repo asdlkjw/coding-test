a3d2ef61aa1c9c84a29a0c3d658269b6566322fe
KIMTAEKWAN <twan4220@gmail.com>
2022-04-02 11:03:49 +0900

    Update 타겟넘버.py

--- 고득점kit/DFS-BFS/타겟넘버.py
+++ 고득점kit/DFS-BFS/타겟넘버.py
@@ -19,3 +19,23 @@ def solution(numbers, target):
 
     dfs(numbers[0], 1)
     return result
+
+
+##########################dfs 정석
+
+answer = 0
+def DFS(idx, numbers, target, value):
+    global answer
+    N = len(numbers)
+    if(idx== N and target == value):
+        answer += 1
+        return
+    if(idx == N):
+        return
+
+    DFS(idx+1,numbers,target,value+numbers[idx])
+    DFS(idx+1,numbers,target,value-numbers[idx])
+def solution(numbers, target):
+    global answer
+    DFS(0,numbers,target,0)
+    return answer
