90c647fa76ff4d374bbb62b6db93dc05c48483c1
KIMTAEKWAN <twan4220@gmail.com>
2022-04-06 21:46:42 +0900

    Update 카펫.py

--- 고득점kit/완전탐색/카펫.py
+++ 고득점kit/완전탐색/카펫.py
@@ -1,3 +1,8 @@
 def solution(brown, yellow):
     answer = []
-    return answer
+    result = brown + yellow  
+    for i in range(3,result-2):
+        x, y = (result/i, i)
+        if (x+y)*2 -4 == brown:
+            answer.append([int(x), int(y)])
+    return answer[0]
