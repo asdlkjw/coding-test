6278361b3b4d44398ecda5bcaa469db6aad79b28
KIMTAEKWAN <twan4220@gmail.com>
2022-04-07 23:23:57 +0900

    k번째수

--- 고득점kit/완전탐색/카펫.py
+++ 고득점kit/완전탐색/카펫.py
@@ -6,3 +6,14 @@ def solution(brown, yellow):
         if (x+y)*2 -4 == brown:
             answer.append([int(x), int(y)])
     return answer[0]
+
+
+##########################################
+
+def solution(brown, red):
+    for i in range(1, int(red**(1/2))+1):
+        if red % i == 0:
+            if 2*(i + red//i) == brown-4:
+                return [red//i+2, i+2]
+            
+            
