ab76af4f5e004bd05cffe0570db2860bcf66ce48
KIMTAEKWAN <twan4220@gmail.com>
2022-04-08 22:03:27 +0900

    up

--- 고득점kit/정렬/K번째수.py
+++ 고득점kit/정렬/K번째수.py
@@ -1,3 +1,3 @@
-def solution(array, commands):
-    answer = []
-    return answer
+def solution(array, commands):     
+    return [sorted(array[i[0]-1:i[1]])[i[2]-1] for i in commands]
+    #11:15분 스타트 #11:21분 종료
