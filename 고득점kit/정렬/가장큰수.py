df7c8c1a208453a3f19d15e394faecb980e82879
KIMTAEKWAN <twan4220@gmail.com>
2022-04-08 23:06:47 +0900

    Update 가장큰수.py

--- 고득점kit/정렬/가장큰수.py
+++ 고득점kit/정렬/가장큰수.py
@@ -2,3 +2,5 @@ def solution(numbers):
     numbers = [[(str(num)*4)[:4], len(str(num))] for num in numbers ]
     nums = (sorted(numbers,reverse = True))
     return ''.join([num[0][:num[1]] for num in nums])
+
+# 한자리수를 4자리수로 바꿔주는것이 포인트였다
