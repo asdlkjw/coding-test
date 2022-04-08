4aff7603886d560b998778a837643545fd7856dd
KIMTAEKWAN <twan4220@gmail.com>
2022-04-08 23:11:15 +0900

    Update 가장큰수.py

--- 고득점kit/정렬/가장큰수.py
+++ 고득점kit/정렬/가장큰수.py
@@ -1,6 +1,6 @@
 def solution(numbers):
     numbers = [[(str(num)*4)[:4], len(str(num))] for num in numbers ]
     nums = (sorted(numbers,reverse = True))
-    return ''.join([num[0][:num[1]] for num in nums])
+    return str(int(''.join([num[0][:num[1]] for num in nums])))
 
-# 한자리수를 4자리수로 바꿔주는것이 포인트였다
+# 한자리수를 4자리수로 바꿔주는것이 포인트였다  int str 다시 써주는건 0000 같은것을 0으로
