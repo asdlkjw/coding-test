f063d01e955538f1abb2e14cf2ba71eb714d4db1
KIMTAEKWAN <twan4220@gmail.com>
2022-04-10 16:30:07 +0900

    Update H-Index.py

--- 고득점kit/정렬/H-Index.py
+++ 고득점kit/정렬/H-Index.py
@@ -1,3 +1,16 @@
 def solution(citations):
-    answer = 0
-    return answer
+    import numpy as np
+    citations = sorted(citations)
+    median = (np.median(citations))
+    w = (citations.index(median))
+    while True:
+        down, up = False, False
+        if citations[w] <= len(citations[w:]):
+            w -= 1
+            down = True
+        if citations[w] >= len(citations[:w]):
+            w += 1
+            up = True
+        if (down == True) and (up == True):
+            return citations[w]
+    
