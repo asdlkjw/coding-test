6cdc8b115295566902682f7f7f296dec1870d3aa
KIMTAEKWAN <twan4220@gmail.com>
2022-04-11 22:17:10 +0900

    Update 네트워크.py

--- 고득점kit/DFS-BFS/네트워크.py
+++ 고득점kit/DFS-BFS/네트워크.py
@@ -3,7 +3,16 @@ def solution(progresses, speeds):
     import math
     pro = [100-i for i in progresses]
     speed = ([math.ceil(i/j) for i,j in zip(pro, speeds)])
-    print(speed)
-    
-    
+    speed.append(101)
+    tmp = []
+    for i,j in enumerate(speed):
+        if i == 0 :
+            tmp.append(j)
+            continue
+        if tmp[0] >= j:
+            tmp.append(j)
+        elif tmp[0] < j:
+            answer.append(len(tmp))
+            tmp = []
+            tmp.append(j)
     return answer
