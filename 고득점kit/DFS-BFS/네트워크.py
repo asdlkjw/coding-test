f9d4a1b6fdb9c292c064dc96884386a9afcaf949
KIMTAEKWAN <twan4220@gmail.com>
2022-04-11 22:04:59 +0900

    Update 네트워크.py

--- 고득점kit/DFS-BFS/네트워크.py
+++ 고득점kit/DFS-BFS/네트워크.py
@@ -1,3 +1,9 @@
-def solution(n, computers):
-    answer = 0
+def solution(progresses, speeds):
+    answer = []
+    import math
+    pro = [100-i for i in progresses]
+    speed = ([math.ceil(i/j) for i,j in zip(pro, speeds)])
+    print(speed)
+    
+    
     return answer
