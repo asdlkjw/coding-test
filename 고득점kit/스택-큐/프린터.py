f0c5aae31914c207b94b5c8648a0c6cc4f92f0b6
KIMTAEKWAN <twan4220@gmail.com>
2022-04-12 21:25:09 +0900

    Update 프린터.py

--- 고득점kit/스택-큐/프린터.py
+++ 고득점kit/스택-큐/프린터.py
@@ -1,3 +1,24 @@
+from collections import deque
 def solution(priorities, location):
     answer = 0
+    my_print = priorities[location]
+    queue = deque(priorities)
+    tmp = 0
+    for i,j in enumerate(priorities):
+        if j < max(list(queue)[1:]):
+            if i == location:
+                tmp += len(priorities[i:])
+            else:
+                # tmp -= 1
+                pass
+            
+            queue.append(j)
+            priorities.append(j)
+            queue.popleft()
+        else:
+            if i == location:
+                tmp += 1
+                break
+            print(j)
+    print(tmp)
     return answer
