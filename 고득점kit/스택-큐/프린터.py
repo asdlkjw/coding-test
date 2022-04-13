ea0ff2f7c14f62155ffb5624ac9462b77211c011
KIMTAEKWAN <twan4220@gmail.com>
2022-04-13 21:43:13 +0900

    Update 프린터.py

--- 고득점kit/스택-큐/프린터.py
+++ 고득점kit/스택-큐/프린터.py
@@ -22,3 +22,30 @@ def solution(priorities, location):
             print(j)
     print(tmp)
     return answer
+
+
+
+import collections
+def popleft_loc_prior(locations_dict, priorities_dict):
+    return locations_dict.popleft(), priorities_dict.popleft()
+
+def append_loc_prior(locations_dict, priorities_dict, loc, prior):
+    locations_dict.append(loc)
+    priorities_dict.append(prior)
+
+def solution(priorities, location):
+    locations_dict  = collections.deque([i for i in range(len(priorities))])
+    priorities_dict = collections.deque(priorities)
+
+    i_print = 0
+    while True:
+        loc, prior = popleft_loc_prior(locations_dict, priorities_dict)
+        if len(priorities_dict) != 0:
+            if prior < max(priorities_dict):
+                append_loc_prior(locations_dict, priorities_dict, loc, prior)
+            else:
+                i_print = i_print + 1
+                if loc == location:
+                    return i_print
+        else:
+            return i_print + 1
