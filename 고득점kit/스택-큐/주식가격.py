30c884f70bf33600d6cd90ff04cb859fa58197aa
KIMTAEKWAN <twan4220@gmail.com>
2022-04-22 00:11:14 +0900

    Update 주식가격.py

--- 고득점kit/스택-큐/주식가격.py
+++ 고득점kit/스택-큐/주식가격.py
@@ -1,3 +1,11 @@
 def solution(prices):
     answer = []
+    room  = [0]*len(prices)
+    for i in range(len(prices)):
+        for price in prices[i+1:]:
+            if i <= price:
+                room[i] += 1
+            else:
+                continue
+    print(room)
     return answer
