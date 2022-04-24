4c2bc5ec5d3f0c272b382bbbb37962fe9b6d544b
KIMTAEKWAN <twan4220@gmail.com>
2022-04-24 22:18:20 +0900

    Update 체육복.py

--- 고득점kit/탐욕법/체육복.py
+++ 고득점kit/탐욕법/체육복.py
@@ -1,3 +1,12 @@
 def solution(n, lost, reserve):
-    answer = 0
+    answer = n - len(lost)
+    tmp = []
+    for i in (reserve):
+        tmp.append(i-1)
+        tmp.append(i+1)
+        
+    for i in lost:
+        if (tmp.count(i)) > 0:
+            answer += 1
+    
     return answer
