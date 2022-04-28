a24b75890e8c0a5aa539e2db960ce4f3ae788a83
KIMTAEKWAN <twan4220@gmail.com>
2022-04-28 22:22:51 +0900

    Update 체육복.py

--- 고득점kit/탐욕법/체육복.py
+++ 고득점kit/탐욕법/체육복.py
@@ -7,3 +7,16 @@ def solution(n, lost, reserve):
         elif i+1 in set_lost:
             set_lost.remove(i+1)#
     return n - len(set_lost)
+
+#또 다른 풀이법
+def solution(n, lost, reserve):
+    _reserve = [r for r in reserve if r not in lost]
+    _lost = [l for l in lost if l not in reserve]
+    for r in _reserve:
+        f = r - 1
+        b = r + 1
+        if f in _lost:
+            _lost.remove(f)
+        elif b in _lost:
+            _lost.remove(b)
+    return n - len(_lost)
