40445df7831b086b6686b2525ce94317a0a60027
KIMTAEKWAN <twan4220@gmail.com>
2022-05-05 13:04:59 +0900

    Update 구명보트.py

--- 고득점kit/탐욕법/구명보트.py
+++ 고득점kit/탐욕법/구명보트.py
@@ -1,3 +1,18 @@
 def solution(people, limit):
-    answer = 0
+    tmp = []
+    for i,weight in enumerate(people):
+        if weight <= (limit/2):
+            tmp.append(weight)
+    for i in tmp:
+        people.pop(people.index(i))
+    answer = len(people)
+    
+    print(people)
+    for l in tmp:
+        for w in people:
+            if w <= (limit - l):
+                answer += 1
+                print('aaaaaa')
+                
+    print(len(tmp)/2)
     return answer
