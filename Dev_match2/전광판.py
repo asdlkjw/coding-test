4f5c4e95c759547696b25f9589c428561e99422d
KIMTAEKWAN <twan4220@gmail.com>
2022-05-15 13:57:02 +0900

    Update 전광판.py

--- Dev_match2/전광판.py
+++ Dev_match2/전광판.py
@@ -1,3 +1,15 @@
 def solution(n, text, second):
-    answer = ''
+    answer = '_'*n
+    text = text.replace(' ', '_')
+    i = 0
+    j = 0
+    while j != second:
+        if i < n:
+            answer = (answer[1:n-i] + text[:i+1])
+        elif i >= n:
+            answer = answer[1:] + '_'*(1)
+        i += 1
+        j += 1
+        if (i % (2*n)) == 0:
+            i = 0
     return answer
