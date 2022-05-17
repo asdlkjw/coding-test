8df3bf7f1242f315360bafaaf9f443716b6644fe
KIMTAEKWAN <twan4220@gmail.com>
2022-05-17 21:32:53 +0900

    Create 베스트앨범.py

---
+++ 고득점kit/해시/베스트앨범.py
@@ -0,0 +1,22 @@
+def solution(genres, plays):
+    answer = []
+    name = {}
+    for i in set(genres):
+        count = []
+        for j in range(len(plays)):
+            if i == genres[j]:
+                count.append(plays[j])
+        name[i] = count
+        
+    q = sorted(name.items(), key = lambda item: sum(item[1]) , reverse = True)
+    
+    pq = []
+    for p in q:
+        pq.append(sorted(p[1], reverse=True))
+    
+    for i in pq:
+        for k in i:
+            for ans,j in enumerate(plays):
+                if k == j:
+                    answer.append(ans)
+    return answer
