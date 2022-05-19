2dd66033ff972f228a4def280894760190d38c13
KIMTAEKWAN <twan4220@gmail.com>
2022-05-20 05:48:46 +0900

    Update 다리를건너는트럭.py

--- 고득점kit/스택-큐/다리를건너는트럭.py
+++ 고득점kit/스택-큐/다리를건너는트럭.py
@@ -1,4 +1,16 @@
 def solution(bridge_length, weight, truck_weights):
     answer = 0
-    bridge_length/2
+    import queue
+    que = queue.Queue()
+    now = 0
+    for truk in truck_weights:
+        if weight - now <= truk:
+            que.put(truk)
+            
+        
+    for _ in range(len(truck_weights)):
+        # (que.get())
+        pass
+    
+    print(sum(que))
     return answer
