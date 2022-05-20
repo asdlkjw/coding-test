58dfcc2fb7cf8c277feab197119c05492c067470
KIMTAEKWAN <twan4220@gmail.com>
2022-05-20 19:47:23 +0900

    Update 더 맵게.py

--- 고득점kit/힙/더 맵게.py
+++ 고득점kit/힙/더 맵게.py
@@ -1,3 +1,11 @@
 def solution(scoville, K):
     answer = 0
+    scoville = sorted(scoville)
+    for i in range(0,len(scoville),2):
+        try:
+            # print(scoville[i], scoville[i+1])
+            pass
+        except:
+            # print(scoville[i])
+            pass
     return answer
