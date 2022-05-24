b6894d28bf1b6de0bee8f3f30cd7ffe98b0ad14e
KIMTAEKWAN <65640292+asdlkjw@users.noreply.github.com>
2022-05-24 19:37:10 +0900

    Update 조이스틱.py
    update

--- 고득점kit/탐욕법/조이스틱.py
+++ 고득점kit/탐욕법/조이스틱.py
@@ -1,4 +1,20 @@
 def solution(name):
-    answer = 0
-    print(name)
+    if set(name) == {'A'}:
+        return 0
+
+    answer = 20*26
+    for i in range(len(name) // 2):
+        left_moved = name[-i:]+name[:-i]
+        right_moved = name[i:]+name[:i]
+        for n in [left_moved, right_moved[0]+right_moved[:0:-1]]:
+            while n and n[-1] == 'A':
+                n = n[:-1]
+
+            row_move = i + len(n)-1
+            col_move = 0
+            for c in map(ord, n):
+                col_move += min(c - 65, 91 - c)
+
+            answer = min(answer, row_move + col_move)
+    print(answer)
     return answer
